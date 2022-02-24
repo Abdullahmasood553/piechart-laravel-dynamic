234786b40c4caccf621fb6fde7ee5736d19f24fc
Abdullah Masood <amasood@octans.ai>
2022-02-24 22:13:31 +0500

    First Commit

---
+++ resources/views/chart.blade.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          <?php echo $chartData ?>
+        ]);
+
+        var options = {
+          title: 'Gender Charts'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
